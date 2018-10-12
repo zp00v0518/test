9f71fa95d6fdb751e27e7a0084afede9af597019
Dmitriy <you@example.com>
2018-10-12 15:17:32 +0300

    first

--- myModules/postMethod.js
+++ myModules/postMethod.js
@@ -4,7 +4,6 @@ const reqOn = require ("./tube").reqOn;
 
 function postMethod(req,res, urlParse) {
 	// console.log("********** PostMethod Work ************");
-	if (urlParse.pathname == "/index.html") {
 		var data ="";
 		req.on('data', function(chunk){
 			data += chunk;
@@ -12,24 +11,9 @@ function postMethod(req,res, urlParse) {
 		req.on("end", function(){
 			// console.log(data)
 			res.writeHead(200, {"Content-Type": "text/plain"});
-			res.end("Very good");
+			res.end(data);
 
 		})
-	} else if (urlParse.pathname == "/subCategory") {
-		var data ="";
-		req.on('data', function(chunk){
-			data += chunk;
-		})
-		req.on("end", function(){
-			searchDB(data, function(result){
-				// console.log("***************Вывод в коллбаке***********");
-				res.writeHead(200, {"Content-Type": "application/json"});
-				res.end(result);
-			});
-		
-
-		})
-	}
 }
 
 module.exports = postMethod;
