c656c45c9ceaae4505198ff6714f7bd19f7834d1
Dmitriy <you@example.com>
2018-10-16 14:05:20 +0300

    test vidjet2

--- js/test.js
+++ js/test.js
@@ -1,1 +1,20 @@
-console.log("test.js")
+	const div = document.createElement('div');
+	const target = document.getElementById('one');
+	const mutationConfig = {
+		childList: true,
+		subtree: true,
+		characterData: true,
+	}
+
+	const observer = new MutationObserver(function(mutations){
+		  const elem = mutations[0].addedNodes[0]
+		  appendTG(elem)
+	});
+	observer.observe(target, mutationConfig)
+
+function appendTG(elem){
+	const clone = elem.cloneNode(true)
+	const next = document.getElementById('two');
+	next.appendChild(clone)
+}
+
