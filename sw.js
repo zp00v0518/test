a32b91496165ceb76ff16dc0c9bb22ce59b0320a
Dmitriy <you@example.com>
2019-04-28 14:42:28 +0300

    add manifest

---
+++ sw.js
@@ -0,0 +1,34 @@
+const cacheName = 'cache-v9';
+const resourcesToPrecache = [
+  '/',
+  'index.html',
+  'style/style.css',
+  'js/Canvas.js',
+  'js/game.js',
+  'js/Keys.js',
+  'js/sourceLoader.js',
+  'favicon.ico',
+  'js/liveReload.js',
+  'img/zmei.png',
+];
+
+self.addEventListener('install', event => {
+  console.log('Install event');
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(resourcesToPrecache);
+    }),
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('activate event', event);
+});
+
+self.addEventListener('fetch', event => {
+  // console.log('fetch intercepted for', event);
+  event.respondWith(caches.match(event.request).then(cachedResponse => {
+    return cachedResponse || fetch(event.request);
+  }).catch(err => console.log(err))
+  );
+});
