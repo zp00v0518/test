9006255d9c806820d5322b4e5e967264930991ff
Dmitry <dmitry.tarabanov@decimadigital.com>
2019-05-05 18:59:06 +0300

    fix test

--- js/push.js
+++ js/push.js
@@ -1,31 +1,47 @@
-var button = document.getElementById("notifications");
+var button = document.getElementById('notifications');
 
 button.addEventListener('click', function(e) {
-  requestPush().then(result =>{
-    showNotification();
-  }).catch(result =>{
-    alert(result)
-  })
+  requestPush()
+    .then(result => {
+      getServiceWorker().then(sw => {
+        showNotification(sw);
+        setTimeout(() => {
+          showNotification(sw);
+        }, 10000);
+      })
+    })
+    .catch(result => {
+      alert(result);
+    });
 });
 
-function requestPush(){
+function requestPush() {
   return new Promise((resolve, reject) => {
     Notification.requestPermission().then(function(result) {
-        if(result === 'granted') {
-            resolve(result);
-        } else {
-          reject(result)
-        }
+      if (result === 'granted') {
+        return resolve(result);
+      } else {
+        return reject(result);
+      }
     });
-  })
-
+  });
 }
 
-function showNotification() {
+function showNotification(sw) {
   const options = {
-    body:  "world", 
-    icon: "zmei.png",
-    vibrate: [100, 50, 100]
-  }
-  var notif = new Notification("Hello", options);
+    body: 'world',
+    icon: 'zmei.png',
+    vibrate: [100, 50, 100],
+  };
+  sw.showNotification('hello', options);
+}
+
+function getServiceWorker() {
+  return new Promise((resolve, reject) => {
+    navigator.serviceWorker.getRegistration()
+      .then(reg => {
+        return resolve(reg);
+      })
+      .catch(err => reject(err));
+  });
 }
