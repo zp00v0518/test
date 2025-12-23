4ee0bda0911f9062346f078b2c2807be935b325e
Dmytro <johndoe@example.com>
2025-12-23 13:19:46 +0200

    аdd 2k

---
+++ _nuxt/D7SQQHqE.js
@@ -0,0 +1,1 @@
+import{D as a,a as s,E as r,F as u,G as o}from"./BBm4iw_q.js";function d(t){const e=t||s();return e.ssrContext?.head||e.runWithContext(()=>{if(r()){const n=u(o);if(!n)throw new Error("[nuxt] [unhead] Missing Unhead instance.");return n}})}function i(t,e={}){const n=e.head||d(e.nuxt);return a(t,{head:n,...e})}export{i as u};
--- js/Keys.js
+++
@@ -1,8 +0,0 @@
-var Keys = {
-  nowPressName: '',
-  nowPressCode: -1,
-  ArrowUp: 38,
-  ArrowDown: 40,
-  ArrowLeft: 37,
-  ArrowRight: 39,
-};
